14d3fed0098a4962762790408a7b7cffc41264c0
Potion24 <acpc6128@gmail.com>
2021-05-19 22:53:41 +0200

    Mon premier commit
    Ajout de "mainScene"

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+
+public class PlayerController : MonoBehaviour
+{
+   [SerializeField]
+   private float speed = 3f;
+
+   private PlayerMotor motor;
+
+   private void Start()
+   {
+       motor = GetComponent<PlayerMotor>();
+   }
+
+   private void Update()
+   {
+       // calcul
+       float xMove = Input.GetAxisRaw("Horizontal");
+       float zMove = Input.GetAxisRaw("Vertical");
+
+       // applique
+       Vector3 moveHorizontal = transform.right * xMove;
+       Vector3 moveVertical = transform.forward * zMove;
+
+       // resultat
+       Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;
+
+       // appel
+       motor.Move(velocity);
+       
+   }
+}
