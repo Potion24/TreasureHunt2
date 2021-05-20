d5c4c4afa10cb0d0eeef13795a15f4b8abf246af
Potion24 <acpc6128@gmail.com>
2021-05-20 19:32:48 +0200

    20/05/2021
    Player + camera

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam;
+
+    public float speed = 10f;
+
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            // calcul la direction général du joueur + le eulerAngles de l'axe y
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            // applique la direction du joueur en fonction de l'angle de la camera
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            // applique la méthode pour lié la direction joueur/camera
+            Vector3 movDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(movDir.normalized * speed * Time.deltaTime);
+        }
+
+    }
+}
